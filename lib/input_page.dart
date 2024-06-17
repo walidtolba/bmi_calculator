9c56ee7e30be320404eb0f17eb45a7ab5e226f5f
Walid Tolba <wrt0624@gmail.com>
2024-06-17 15:34:56 +0100

    second

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,7 +16,9 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   GenderType? genderSelected;
-  int height = 150;
+  int height = 170;
+  int weight = 70;
+  int age = 25;
 
   void updateGender(GenderType gender) {
     genderSelected = (genderSelected != gender) ? gender : null;
@@ -68,7 +70,6 @@ class _InputPageState extends State<InputPage> {
               color: kActiveReusableCardColor,
               onTap: () {},
               child: Column(
-                
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
@@ -92,22 +93,24 @@ class _InputPageState extends State<InputPage> {
                   ),
                   SliderTheme(
                     data: SliderTheme.of(context).copyWith(
-                      activeTickMarkColor: Colors.white,
-                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                      thumbColor: kBottomContainerColor,
-                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
-                      overlayColor: kBottomContainerColor.withAlpha(0x29),
-                      trackHeight: 1
-                    ),
+                        activeTickMarkColor: Colors.white,
+                        thumbShape:
+                            RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        thumbColor: kBottomContainerColor,
+                        overlayShape:
+                            RoundSliderOverlayShape(overlayRadius: 30.0),
+                        overlayColor: kBottomContainerColor.withAlpha(0x29),
+                        trackHeight: 1),
                     child: Slider(
-                        value: height.toDouble(),
-                        onChanged: (value) {
-                          setState(() {
-                            height = value.toInt();
-                          });
-                        },
-                        min: 120,
-                        max: 220, ),
+                      value: height.toDouble(),
+                      onChanged: (value) {
+                        setState(() {
+                          height = value.toInt();
+                        });
+                      },
+                      min: 120,
+                      max: 220,
+                    ),
                   ),
                 ],
               ),
@@ -120,12 +123,72 @@ class _InputPageState extends State<InputPage> {
                   child: ReusableCard(
                     color: kActiveReusableCardColor,
                     onTap: () {},
+                    child: Column(
+                      children: [
+                        Text(
+                          'WEIGHT',
+                          style: kLabelTextStyle,
+                        ),
+                        Text(weight.toString(), style: kBigNumberTextStyle),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            MyCustomButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              child: Icon(FontAwesomeIcons.plus),
+                            ),
+                            MyCustomButton(
+                              onPressed: () {
+                                setState(() {
+                                  weight--;
+                                });
+                              },
+                              child: Icon(FontAwesomeIcons.minus),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
                     color: kActiveReusableCardColor,
                     onTap: () {},
+                    child: Column(
+                      children: [
+                        Text(
+                          'AGE',
+                          style: kLabelTextStyle,
+                        ),
+                        Text(age.toString(), style: kBigNumberTextStyle),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            MyCustomButton(
+                              onPressed: () {
+                                setState(() {
+                                  age++;
+                                });
+                              },
+                              child: Icon(FontAwesomeIcons.plus),
+                            ),
+                            MyCustomButton(
+                              onPressed: () {
+                                setState(() {
+                                  age--;
+                                });
+                              },
+                              child: Icon(FontAwesomeIcons.minus),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 )
               ],
@@ -143,6 +206,27 @@ class _InputPageState extends State<InputPage> {
         child: Icon(Icons.add),
         onPressed: () {},
       ),
+    );
+  }
+}
+
+class MyCustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Widget child;
+  MyCustomButton({required this.onPressed, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      elevation: 6.0,
+      onPressed: onPressed,
+      child: child,
+      shape: CircleBorder(),
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      fillColor: Color(0xFF4C4F5E),
     );
   }
 }
